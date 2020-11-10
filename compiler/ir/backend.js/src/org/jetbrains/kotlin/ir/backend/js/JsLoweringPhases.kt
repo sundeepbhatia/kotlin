833516581a3e00447ffa7e9eb54fa74f1c5f7249
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-11-10 15:53:58 +0300

    [JS IR] Migrate on body lowering pass and declaration transformer

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/JsLoweringPhases.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/JsLoweringPhases.kt
@@ -364,9 +364,15 @@ private val forLoopsLoweringPhase = makeBodyLoweringPhase(
     description = "[Optimization] For loops lowering"
 )
 
-private val propertyLazyInitLoweringPhase = makeFileLoweringPhase(
-    ::PropertyLazyInitLowering,
-    name = "PropertyLazyInitLowering",
+private val propertyLazyInitLoweringPhase2 = makeBodyLoweringPhase(
+    ::PropertyLazyInitLowering2,
+    name = "PropertyLazyInitLowering2",
+    description = "Make property init as lazy"
+)
+
+private val nullizeDeclarations = makeDeclarationTransformerPhase(
+    ::NullizeDeclarations,
+    name = "NullizeDeclarations",
     description = "Make property init as lazy"
 )
 
@@ -737,7 +743,8 @@ val loweringList = listOf<Lowering>(
     rangeContainsLoweringPhase,
     forLoopsLoweringPhase,
     primitiveCompanionLoweringPhase,
-    propertyLazyInitLoweringPhase,
+    propertyLazyInitLoweringPhase2,
+    nullizeDeclarations,
     propertyAccessorInlinerLoweringPhase,
     foldConstantLoweringPhase,
     privateMembersLoweringPhase,
